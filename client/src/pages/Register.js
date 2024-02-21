90710fc211745c027f3e3c240e9e821e5be458f4
Rahul <Rahulsingh4533@gmail.com>
2024-02-21 23:47:02 +0530

    adding user and password

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Register() {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="bg-blue-50 flex items-center">
-    <form className='w-64 mx-auto'>
-        <input type='text' placeholder='Username' className='block w-full rounded-sm p-2 mb-2'/>
-        <input type='password' placeholder='password' className='block w-full rounded-sm p-2 mb-2'/>
-        <button className="bg-blue-700 w-full rounded-sm text-white p-2">Register</button>
-    </form>
-
+      <form className="w-64 mx-auto mb-12">
+        <input
+          type="text"
+          placeholder="Username"
+          className="block w-full rounded-sm p-2 mb-2"
+          onChange={(e) => setUser(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="block w-full rounded-sm p-2 mb-2"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="bg-blue-700 w-full rounded-sm text-white p-2">
+          Register
+        </button>
+      </form>
     </div>
-   
-  )
+  );
 }
 
-export default Register
+export default Register;
