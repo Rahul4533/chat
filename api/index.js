767ab6363683a6277190718ddbc45b2c674db779
Rahul <Rahulsingh4533@gmail.com>
2024-02-22 01:46:21 +0530

    adding jwt

--- api/index.js
+++ api/index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const port = 5000;
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const jwt=require('jsonwebtoken');
+const User=require('./models/user');
 dotenv.config();
+const jwtsec=process.env.JWT_SEC;
 mongoose.connect(process.env.MOGO_URL);
 const db=mongoose.connection;
 db.once('open',(err)=>{
@@ -12,6 +15,26 @@ db.once('open',(err)=>{
     console.log("DB Connected");
 })
 const app = express();
+
+
+
+
+app.post('/register',async(req,res) =>{
+
+   const {username,password}=req.body;
+const createdUser=  await User.create({username,password});
+  
+  jwt.sign({userId:createdUser._id},jwtsec,(err,token)=>{
+    if(err){
+     throw err;
+    }
+
+    res.cookie('token',token).status(201).json('ok');
+});
+console.log(req.body);
+  
+
+})
 
 app.listen(port, (err) => {
   if (err) {
