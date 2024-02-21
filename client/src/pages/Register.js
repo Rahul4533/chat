828c9e5b417bdb15b5993966a4eedafa3e8032ba
Rahul <Rahulsingh4533@gmail.com>
2024-02-22 00:15:24 +0530

    register made

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -4,21 +4,24 @@ function Register() {
   const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <div className="bg-blue-50 flex items-center">
+    <div className="bg-blue-50 h-screen  flex items-center">
       <form className="w-64 mx-auto mb-12">
         <input
+
+        value={user}
           type="text"
           placeholder="Username"
           className="block w-full rounded-sm p-2 mb-2"
           onChange={(e) => setUser(e.target.value)}
         />
         <input
+        value={password}
           type="password"
           placeholder="password"
           className="block w-full rounded-sm p-2 mb-2"
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="bg-blue-700 w-full rounded-sm text-white p-2">
+        <button className="bg-blue-700 w-full rounded-lg text-white p-2">
           Register
         </button>
       </form>
