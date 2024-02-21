0df26c6cfd93e5566b61ccd3ce978855416ceea6
Rahul <Rahulsingh4533@gmail.com>
2024-02-22 00:42:39 +0530

    Connected to db

--- api/index.js
+++ api/index.js
@@ -3,8 +3,14 @@ const port = 5000;
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-
 mongoose.connect(process.env.MOGO_URL);
+const db=mongoose.connection;
+db.once('open',(err)=>{
+    if(err){
+        console.log("error while connecting to db "+err.message);
+    }
+    console.log("DB Connected");
+})
 const app = express();
 
 app.listen(port, (err) => {
