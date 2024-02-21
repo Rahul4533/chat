6e17fb71085afe9b4a053488501ba4d182733dea
Rahul <Rahulsingh4533@gmail.com>
2024-02-22 00:33:01 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,15 +1,16 @@
-const express =require("express");
-const port=5000;
+const express = require("express");
+const port = 5000;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const app=express();
+mongoose.connect(process.env.MOGO_URL);
+const app = express();
 
+app.listen(port, (err) => {
+  if (err) {
+    console.log("error while establish the server");
+  }
 
-app.listen(port,(err)=>{
-
-    if(err){
-        console.log("error while establish the server");
-    }
-
-    console.log(`Server Running on PORT ${port} `);
-
-})
+  console.log(`Server Running on PORT ${port} `);
+});
